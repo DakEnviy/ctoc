62f4ab04b7eeaa6d3a1d90a929ec3a4838abbc18
Golbakh Mikhail <golbah@yandex.ru>
2020-05-21 00:10:02 +0300

    Add settings

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+
+export interface SkyWeatherConfig {
+    extensionName: string;
+}
+
+export const config: SkyWeatherConfig = {
+    extensionName: 'skyweather',
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, window } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { settings } from './settings';
+import { getCommandName } from './utils/commands';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+export function activate(context: ExtensionContext) {
+
     console.log('Congratulations, your extension "skyweather" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('skyweather.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
+    context.subscriptions.push(commands.registerCommand(getCommandName('updateSettings'), () => {
+        settings.updateSettings();
+    }));
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from skyweather!');
+    let disposable = commands.registerCommand('skyweather.helloWorld', () => {
+        window.showInformationMessage('Hello World from skyweather!');
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
