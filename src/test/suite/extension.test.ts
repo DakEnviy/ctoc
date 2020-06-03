0cebae763c5275159897f7f27213a4cbad0d5178
Andrew Gnevanov <andreyrnd2411@gmail.com>
2020-06-04 02:48:23 +0300

    Add test for weather service

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,10 +1,11 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import * as sinon from 'sinon';
 import * as vscode from 'vscode';
+
 import { getCommandName } from '../../utils/commands';
-// import * as myExtension from '../../extension';
+import { WeatherApiResponse, WeatherApi } from '../../api/weather';
+import { Weather } from '../../service/weather';
+import { settings } from '../../settings';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
@@ -17,5 +18,37 @@ suite('Extension Test Suite', () => {
     test('Get command name util test', () => {
         const result = getCommandName('TestName');
         assert.equal(result, 'skyweather.TestName');
+    });
+
+    test('Weather service test', async () => {
+        const weatherResponseMock: WeatherApiResponse = {
+            main: {
+                temp: 13,
+            },
+            sys: {
+                country: 'RU'
+            },
+            name: 'Zlatoust',
+        };
+        const settingsMock = {
+            location: 'Zlatoust,RU'
+        };
+
+        const makeRequestStub = sinon.stub(WeatherApi, 'makeRequest');
+        makeRequestStub.withArgs(settingsMock.location).resolves(weatherResponseMock);
+        const settingsStub = sinon.stub(settings, 'location').get(() => settingsMock.location);
+        const onUpdateSpy = sinon.spy();
+
+        const weatherService = new Weather();
+        weatherService.onUpdate(onUpdateSpy);
+        await weatherService.update();
+
+        assert.strictEqual(weatherService.getErrorMessage(), null);
+        assert.strictEqual(weatherService.getTemperature(), 13);
+        assert.strictEqual(weatherService.getCity(), 'Zlatoust,RU');
+        assert.ok(onUpdateSpy.calledOnce);
+
+        makeRequestStub.restore();
+        settingsStub.restore();
     });
 });
